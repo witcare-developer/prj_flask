444255145e114f87fbf57cf2c3cc414e3c38c2ce
Renato <renato@witcare.com.br>
2021-12-15 14:56:21 -0300

    Adiciona sqlalchemy

---
+++ database-manager/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(84), nullable=False)
+    email = db.Column(db.String(84), nullable=False, unique=True, index=True)
+    password = db.Column(db.String(255), nullable=True)
+
+    def __str__(self):
+        return self.name
+
+@app.route("/")
+def index():
+    return "Página em branco"
+
+if __name__ == "__main__":
+    app.run(debug=True)
